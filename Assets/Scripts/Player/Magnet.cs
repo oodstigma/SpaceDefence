00c24e997cdb24b54e533de7e3a2f5e9230ce5ee
Matthew Miller <oodstigma@gmail.com>
2016-02-05 19:13:04 -0800

    first commit

---
+++ Assets/Scripts/Player/Magnet.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : MonoBehaviour
+{
+
+    public GameObject parent;
+
+   private float magnetizeX;
+    private float magnetizeY;
+
+    /*void Start()
+    {
+        parent = GetComponentInParent<GameObject>();
+        Debug.Log(parent.name);
+    }*/
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GetPosition(other);
+        Debug.Log(magnetizeX.ToString());
+        Debug.Log(magnetizeY.ToString());
+        other.transform.parent = parent.gameObject.transform;
+        Vector2 newPos = new Vector2(magnetizeX + parent.transform.position.x, magnetizeY + parent.transform.position.y);
+        other.transform.position = newPos;
+       
+    }
+
+    public void GetPosition(Collider2D other)
+    {
+        Debug.Log("GetPosition");
+        switch (other.name)
+        { 
+            case "Left":
+                magnetizeX = -1.5f;
+                magnetizeY = 0f;
+                Debug.Log("Left");
+                return;
+            case "Down":
+                magnetizeX = 0f;
+                magnetizeY = -1.5f;
+                Debug.Log("down");
+                return;
+            case "Right":
+                magnetizeX = 1.5f;
+                magnetizeY = 0f;
+                Debug.Log("right");
+                return;
+            case "Up":
+                magnetizeX = 0f;
+                magnetizeY = 1.5f;
+                Debug.Log("up");
+                return;
+        }
+    }
+
+}
+
+
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 10f;
+
+    private Vector3 mvVector3;
+    private Rigidbody2D playerRigidbody;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        mvVector3.Set(h, v, 0f);
+
+
+        mvVector3 = mvVector3.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + mvVector3);
+    }
+}
